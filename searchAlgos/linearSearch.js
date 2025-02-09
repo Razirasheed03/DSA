09ef1dde8b6eab83460a93788e6618a636cdde24
Razirasheed03 <razirasheed03@gmail.com>
2025-02-09 11:09:35 +0530

    workouts

--- searchAlgos/linearSearch.js
+++ searchAlgos/linearSearch.js
@@ -12,3 +12,15 @@ const arr = [1,2,3,4,5]
 console.log(linearSearch(arr,0))
 console.log(linearSearch(arr,1))
 
+// 2. Check if an Element Exists in an Array
+function containsElement(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return true; 
+        }
+    }
+    return false; 
+}
+
+// const arr = [9, 10, 11, 40, 50];
+console.log(containsElement(arr, 11)); 
