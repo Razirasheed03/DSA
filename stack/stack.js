9079fc7a50b35f177b90c669f188ce78116bdd48
Razirasheed03 <razirasheed03@gmail.com>
2025-02-14 21:40:17 +0530

    stack using linkedlist

--- stack/stack.js
+++ stack/stack.js
@@ -21,3 +21,45 @@ stacknew.push(10)
 stacknew.push(30)
 console.log(stacknew.peek())
 stacknew.printStack()
+
+
+/////////stack using linked list
+class Node{
+    constructor(value){
+        this.value=value;
+        this.next=null
+    }
+    }
+    class Stack{
+        constructor(){
+            this.top=null;
+            this.size=0
+        }
+        push(element){
+            const newNode=new Node(element)
+            if(this.top){
+                newNode.next=this.top
+            }
+            this.top=newNode
+        }
+        pop(){
+            this.top=this.top.next
+        }
+        display(){
+            let curr=this.top
+            let listValue='';
+            while(curr){
+                listValue+=curr.value;
+                curr=curr.next
+            }
+                console.log(listValue)
+        }
+    }
+    
+    
+    const stack=new Stack()
+    stack.push(10)
+    stack.push(20)
+    stack.pop()
+    stack.display()
+    
