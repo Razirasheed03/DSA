1145656695912373bf0b270745766be2796efb56
Razirasheed03 <razirasheed03@gmail.com>
2025-02-16 19:53:50 +0530

    queue using stack

--- queue/queue.js
+++ queue/queue.js
@@ -18,3 +18,35 @@ class Queue{
 const queue=new Queue()
 queue.push(10)
 queue.push(20)
+
+
+
+
+///////========queue using stack========//////
+
+class queueUsingStack{
+    constructor(){
+        this.stack1=[];
+        this.stack2=[]
+    }
+    push(value){
+        this.stack1.push(value)
+    }
+    pop(){
+        if(this.stack2.length===0){
+            while(this.stack1.length>0){
+                this.stack2.push(this.stack1.pop())
+            }
+        }
+        return this.stack2.pop();
+    }
+    print(){
+        console.log(this.stack2.join(' '))
+    }
+}
+
+// const queue=new queueUsingStack()
+// queue.push(10)
+// queue.push(20)
+// queue.pop()
+// queue.print()
