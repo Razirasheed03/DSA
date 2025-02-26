e8ae2bc99d3a171acf839268c9ec662315e64492
Razirasheed03 <razirasheed03@gmail.com>
2025-02-26 09:13:34 +0530

    string workouts

---
+++ string/string.js
@@ -0,0 +1,9 @@
+function lengthCheck(str){
+    let length=0;
+    while(str[length]){
+        length++
+    }
+    return length
+}
+console.log(lengthCheck('razi'))
+console.log(lengthCheck('nadeem'))
