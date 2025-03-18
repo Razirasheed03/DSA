df48c709a2cfaf18afcf75377628e49903e36205
Razirasheed03 <razirasheed03@gmail.com>
2025-03-18 09:21:23 +0530

    graph datastructure workouts

--- graph/graph.js
+++ graph/graph.js
@@ -20,3 +20,9 @@ class Graph{
 }
 const graph=new Graph()
 graph.addVertex('A')
+graph.addVertex('B')
+graph.addVertex('D')
+graph.addVertex('C')
+graph.addEdge('A','B')
+graph.addEdge('A','C')
+graph.addEdge('A','D')
