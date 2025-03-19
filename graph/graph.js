c21e6cd83e9e2049d2f467258a54bdc407f4d7b3
Razirasheed03 <razirasheed03@gmail.com>
2025-03-19 08:53:53 +0530

    workouts

--- graph/graph.js
+++ graph/graph.js
@@ -17,6 +17,9 @@ class Graph{
         this.adjList[vertex1].add(vertex2)
         this.adjList[vertex2].add(vertex1)
     }
+    hasEdge(vertex1,vertex2){
+        return (this.adjList[vertex1].has(vertex2)&&this.adjList[vertex2].has(vertex1))
+    }
 }
 const graph=new Graph()
 graph.addVertex('A')
