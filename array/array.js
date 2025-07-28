de4983243b9d04fa1a2a8c1ebb3c135a5087c776
Razirasheed03 <razirasheed03@gmail.com>
2025-07-28 23:10:28 +0530

    added string workout

--- array/array.js
+++ array/array.js
@@ -175,3 +175,11 @@ function isPalidrom(str) {
     return false
 }
 // console.log(isPalidrom('mada'))
+
+
+let str='razi pp'
+let result=str[0].toUpperCase()
+for(let i=1;i<str.length;i++){
+    result+=str[i]
+}
+console.log(result)
